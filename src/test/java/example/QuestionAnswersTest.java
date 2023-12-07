d11c2e219b30e3da314afd62e356db1726b58c74
Josef <mjosef@gmx.at>
2023-12-07 20:36:16 +0100

    refactor

--- src/test/java/example/QuestionAnswersTest.java
+++ src/test/java/example/QuestionAnswersTest.java
@@ -10,7 +10,7 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class QuestionAnswers_Test {
+public class QuestionAnswersTest {
     private QuestionAnswers qa;
 
     @BeforeEach
